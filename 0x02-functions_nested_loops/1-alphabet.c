de1ba44727d16026b87b52777ad29a188f0fa1fd
Youanna <youannasherif@gmail.com>
2023-07-18 17:30:36 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,17 @@
 
 /**
  * alphabet - types the alphabet in lowercase letters to the stdout
+ *
+ * Descreption: write the alphabet
  * 
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet(void
 {
 int i;
-for (i=97;i<=122;i++)
+for (i= 97; i<= 122; i++)
 {
-_putchar(i);	
+_putchar(i);
 }
 _putchar('\n');
 }
