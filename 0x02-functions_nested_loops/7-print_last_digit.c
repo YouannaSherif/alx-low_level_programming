5f6766972ced670df7ae7422eee0941ce9ad89d7
Youanna <youannasherif@gmail.com>
2023-07-23 03:50:40 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit -prints last digit of a number
+ *
+ * @x: is a parameter
+ *
+ * Return: 0 on success
+ */
+
+int print_last_digit(int x)
+{
+	x %= 10;
+	return (x);
+}
