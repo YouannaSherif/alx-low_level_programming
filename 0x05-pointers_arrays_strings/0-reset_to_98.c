85f2bfbdaa4e02edb8a2fbaa03e357cd0da152ca
Youanna <youannasherif@gmail.com>
2023-07-24 06:33:59 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void reset_to_98 -reset the value to 98 by a pointer
+ *
+ * @n: is a pointer
+ *
+ * Return: 0 on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
