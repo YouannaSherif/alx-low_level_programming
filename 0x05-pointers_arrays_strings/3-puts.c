eae66ae249c7436ebdec2052c11621179975108f
Youanna <youannasherif@gmail.com>
2023-07-24 07:21:21 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts -prints string to stdout followed by a new line
+ *
+ * @str: a pointer
+ *
+ * Return: 0 on success
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
