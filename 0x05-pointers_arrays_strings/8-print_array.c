6e2a517753f888dbb2073f04393b31ed5f5b3db3
Youanna <youannasherif@gmail.com>
2023-07-25 16:38:32 -0700

    print an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n > 0)
+	{	
 	for (i = 0; i <= (n - 2); i++)
 		printf("%d, ", a[i]);
 	printf("%d", a[n - 1]);
 	putchar('\n');
+	}
+	else
+		putchar('\n');
 }
