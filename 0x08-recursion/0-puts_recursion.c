39e71e85411ffa16e9fd67a42f5afaaf95fbb0b6
Youanna <youannasherif@gmail.com>
2023-08-03 11:48:25 -0700

    display string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts_recursion -prints a string followed by a new line
+ *
+ * @s: a poiner
+ *
+ * Return: 0 on success
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+	_putchar(s[i++]);
+	}
+	_putchar('\n');
+}
