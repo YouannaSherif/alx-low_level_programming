c957f956a64d274d85d35d1ded4c18f08a62aac0
Youanna Sherif <youannasherif@gmail.com>
2023-08-09 06:10:00 -0700

    putting a string in a pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup -puts a string into a pointer
+ *
+ * @str: a pointer
+ *
+ * Return: a pointer
+ * On failing it returns NULL
+ */
+
+char *_strdup(char *str)
+{
+	int size = strlen(str);
+	char *ptr = (char *) malloc(size * sizeof(str));
+	int i;
+
+	if (ptr == NULL)
+		return (NULL);
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+			ptr[i] = str[i];
+		return (ptr);
+	}
+	else
+		return (NULL);
+}
