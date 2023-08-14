eed10edd7b5cb22243d8935d18e007bb5c291d3e
Youanna Sherif <youannasherif@gmail.com>
2023-08-14 05:57:12 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_H
+#define dog_H
+/**
+ * struct dog -a dog's struct
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
