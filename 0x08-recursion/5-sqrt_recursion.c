85dd12078e01ff3f651632ddb456d61f52eb4064
Youanna Sherif <youannasherif@gmail.com>
2023-08-15 09:36:46 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,19 +5,20 @@
  * _sqrt_recursion -the square root of a number
  *
  * @n: a parameter
+ * @var: a parameter
  *
  * Return: the square root
  * -1 if the number doesn't have a square root
  */
 
-int sqrt(int n, int var);
+int sq(int n, int var);
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n, 1));
+	return (sq(n, 1));
 }
 
 /**
- * sqrt -helps to find the square root
+ * sq -helps to find the square root
  *
  * @n: a parameter
  * @var: a parameter
@@ -26,12 +27,12 @@ int _sqrt_recursion(int n)
  * -1 on failing
  */
 
-int sqrt(int n, int var)
+int sq(int n, int var)
 {
 		if (n == var)
 			return (var);
 		if (n < var)
-			return (sqrt(n, var++));
+			return (sq(n, var++));
 		else
 			return (-1);
 }
