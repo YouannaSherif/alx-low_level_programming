1c0ad81fdbd3972708434c2a40fa3cc8f9ad79dc
Youanna Sherif <youannasherif@gmail.com>
2023-08-16 15:21:38 -0700

    small calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * main -Entery point
+ *
+ * @argc: the number of args
+ * @argv: arg vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int (*opf)(int, int), a, b;
+
+	if (argc != 4)
+		printf("Error"), exit(98);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	opf = get_op_func(argv[2]);
+
+	if (!opf)
+		printf("Error"), exit(99);
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{printf("Error"), exit(100); }
+
+			printf("%d\n", opf(a, b));
+			return (0);
+}
