ef7b6ac38926bad528c6fb039ab280ea9f58445c
Youanna Sherif <youannasherif@gmail.com>
2023-08-16 12:19:51 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name -print names
+ *
+ * @name: points to string
+ * @f: points to a function
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
