ba35218ca63c1eca30a6935532f7b4b3560da8a6
Youanna Sherif <youannasherif@gmail.com>
2023-08-24 03:59:14 -0700

    argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -Entery point
+ * @argc: counter
+ * @argv: array of pointers
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+		printf("%s\n", argv[argc - 1]);
+	return (0);
+}
