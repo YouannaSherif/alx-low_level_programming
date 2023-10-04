a64395c6c7e64923aa2f75a08c986ae49572ed08
Youanna Sherif <youannasherif@gmail.com>
2023-10-04 10:52:55 -0700

    string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,15 +9,14 @@
 
 char *_strdup(char *str)
 {
-	unsigned int i, size = strlen(str);
+	unsigned int i, size = strlen(str) + 1;
 	char *ptr = (char *) malloc(size * sizeof(char));
 
 	if (str && ptr)
 	{
 		for (i = 0; i < size; i++)
 			ptr[i] = str[i];
-		i++;
-		ptr[i] = putchar('\0');
+		ptr[size] = putchar('\0');
 		return (ptr);
 	}
 	return (NULL);
