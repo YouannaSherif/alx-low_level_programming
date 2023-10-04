9edaa532fa103fc8dc1ab66d0c0ac84fbe359380
Youanna Sherif <youannasherif@gmail.com>
2023-10-04 10:45:35 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,16 +3,16 @@
 /**
  * _strdup -returns a pointer to a newly allocated space in memory
  * @str: string
- * Returns: a string on success
+ * Return: a string on success
  * Null on failing
  */
 
 char *_strdup(char *str)
 {
-	unsigned int i, size = strlen(str);
+	unsigned int i, size = strlen(str) - 1;
 	char *ptr = (char *) malloc(size * sizeof(char));
 
-	if (str && ptr)
+	if (str != NULL && ptr)
 	{
 		for (i = 0; i < size; i++)
 			ptr[i] = str[i];
