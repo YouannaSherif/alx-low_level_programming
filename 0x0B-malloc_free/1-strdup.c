a09e3cc3649fa0e954b89c1fd0efbfb7a3ebf5ba
Youanna Sherif <youannasherif@gmail.com>
2023-10-04 10:49:34 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,13 +9,15 @@
 
 char *_strdup(char *str)
 {
-	unsigned int i, size = strlen(str) - 1;
+	unsigned int i, size = strlen(str) + 1;
 	char *ptr = (char *) malloc(size * sizeof(char));
 
-	if (str != NULL && ptr)
+	if (str && str != NULL && ptr)
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 			ptr[i] = str[i];
+		i++;
+		ptr[i] = putchar('\0');
 		return (ptr);
 	}
 	return (NULL);
